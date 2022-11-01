fe8ea2584a415af55dc4d9ba795552861415f5e4
Chris <chrishethertonweb@gmail.com>
2022-11-01 12:05:25 +0100

    added bootstrap components and set bootstrap card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import {useContext, useEffect} from 'react'
+import { Context } from './components/Context';
+import Card from './components/Card';
 
 function App() {
+
+  const {state,dispatch} = useContext(Context)
+
+  useEffect(() => {
+
+    const getData = async () => {
+      const response = await fetch('https://fakestoreapi.com/products')
+
+      const data = await response.json()
+      console.log(data)
+
+      dispatch({type: 'addProducts', payload: data})
+    }
+    getData()
+
+  },[])
+  console.log('state', state)
+
   return (
     <div className="App">
-      DIV
+      <Card/>
     </div>
   );
 }
---
+++ src/components/Context.js
@@ -0,0 +1,46 @@
+import {createContext, useReducer} from 'react'
+
+export const Context = createContext()
+
+
+ function ContextProvider({children}) {
+
+    const reducer = (state,action) => {
+
+        switch(action.type) {
+
+                case 'addProducts':
+
+                return {
+                    ...state,
+                    products: [...action.payload]
+                }
+
+                case 'addCart':
+                return
+
+                case 'removeCart':
+                return 
+                         
+                case 'search':
+                return
+
+                default:
+                    return
+        }
+    }
+
+    const [state, dispatch]  = useReducer(reducer,{
+        products:[],
+        cart: [],
+        searchtext:''    
+    })
+
+
+  return (
+    <Context.Provider value={{state, dispatch}}>
+            {children}
+    </Context.Provider>
+  )
+}
+export default ContextProvider
